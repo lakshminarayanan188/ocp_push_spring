d5bf3c20792034fefb1e8a6749dd67d206b12be6
lakshminarayanan <lakshminarayanan188@gmail.com>
2024-09-07 15:25:42 +0530

    First Commit

---
+++ git_import_sample/src/main/java/com/example/demo/GitImportSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitImportSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitImportSampleApplication.class, args);
+	}
+
+}
